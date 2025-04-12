3058362d5edd4d49e51275ceadd68213af3ee19e
koutsopoulosw <koutsopoulosw@gmail.com>
2025-04-12 12:49:47 -0700

    Converting layout to flex and messing with CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,42 +4,17 @@ import Resume from "./ui/resume";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr] items-center justify-items-start min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <header className="mb-4 mt-12 justify-start">
+    <div className="flex flex-col items-center justify-items-start min-h-screen gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <header className="mb-4 justify-start w-full">
         <h1 className="text-4x1 md:text-5xl lg:text-6xl font-extrabold leading-none pb-6">Alex Koutsopoulos</h1>
         <h2 className="text-2x1 md:text-3xl lg:text-4xl pb-2">Full Stack Engineer</h2>
         <h3 className="text-l md:text-xl lg:text-2xl text-slate-400 ml-8">Also known as Willard</h3>
       </header>
       <section className="flex flex-row gap-8 w-full justify-between">
-        <main className="flex flex-col gap-8 items-center sm:items-start justify-self-start">          
+        <main className="flex flex-col flex-4 gap-4 ml-12 items-center sm:items-start justify-self-start">          
           <Resume />
-          <div className="flex gap-4 items-center flex-col sm:flex-row">
-            <a
-              className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image
-                className="dark:invert"
-                src="/vercel.svg"
-                alt="Vercel logomark"
-                width={20}
-                height={20}
-              />
-              Deploy now
-            </a>
-            <a
-              className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-              href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Read our docs
-            </a>
-          </div>
         </main>
-        <footer className="flex flex-col gap-12 flex-wrap items-end justify-center justify-self-end">
+        <footer className="flex flex-col flex-1 gap-12 flex-wrap items-end justify-center justify-self-end">
           <a
             className="flex items-center gap-2 hover:underline hover:underline-offset-4"
             href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
